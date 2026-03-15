08eb9e706afd7753aa47c71e6c262801f4c8beb5
Ganesh Nemmani <ganeshnemmani@varenyas-macbook-pro.local>
2026-03-15 13:14:24 -0400

    Initial Xcode project

---
+++ Mathrubhasha-iOS/Mathrubhasha_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mathrubhasha_iOSApp.swift
+//  Mathrubhasha-iOS
+//
+//  Created by Ganesh Nemmani on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Mathrubhasha_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
